f6a945ba9354cd6bb646fe8030fa8f1ce9908e1d
mus092tafa <asas.mm2000@gmail.com>
2023-07-29 12:59:10 +0300

    rami ma yftehm

--- lib/main.dart
+++ lib/main.dart
@@ -112,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () => _startAddNewTx(context), icon: Icon(Icons.add))
       ],
       title: Text(
-        'rami m6e',
+        'Last change',
         style: Theme.of(context).textTheme.titleLarge,
       ),
     );
